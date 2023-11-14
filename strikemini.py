f0b69c918eb0e80c2011c36355d087b26b6db0d2
cesperus <148219893+cesperus@users.noreply.github.com>
2023-11-14 08:33:18 -0500

    trying to make baclground

--- strikemini.py
+++ strikemini.py
@@ -4,6 +4,35 @@ from playerA import Player1
 from playerB import Player2
 from bomb import Bomb
 
+def build_background(screen):
+
+    # returns pygame surface that will be the background for the game
+
+    # define each tile we will be using
+    full_dirt = pygame.image.load('assets/ground_textures/')
+    edge_dirt =
+    wall_dirt =
+
+    # get size and width of game screen
+
+    screen_width = screen.get_width()
+    screen_height = screen.get_height()
+
+    # make a surface that will be the background
+
+    bg = pygame.Surface((screen_width, screen_height))
+    bg.fill (0,0,88)
+
+    # get tile size
+
+    # build matrix to define where all tiles go
+
+    # define default value to pad the track with
+
+    # loop over each piece of matrix and blit it to the surface
+
+    # return the surface we made
+
 
 # pygame setup
 pygame.init()
